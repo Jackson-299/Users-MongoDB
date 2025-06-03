1ddb7cd4e3492edff6045be0812fdf29ee783942
Jackson-299 <havocjack2942002@gmail.com>
2025-06-03 12:23:19 +0530

    update

--- server.js
+++ server.js
@@ -115,6 +115,12 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
+
+
 const uri = process.env.MONGODB_URI;
 
 mongoose
